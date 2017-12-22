34dfd71093415b47a8bbe8befba7a62d3ce713c0
grady <gongyu649424146@163.com>
2017-12-22 23:38:00 +0800

    spring

--- spring-freemarker/src/main/java/com/Application.java
+++ spring-freemarker/src/main/java/com/Application.java
@@ -1,10 +1,17 @@
 package com;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @Description:
  * @Todo:
  * @Author Grady
  * Created on 2017/12/12.
  */
+@SpringBootApplication
 public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
 }
--- spring-freemarker/src/main/java/com/Controller.java
+++ spring-freemarker/src/main/java/com/Controller.java
@@ -1,10 +1,25 @@
 package com;
 
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @Description:
  * @Todo:
  * @Author Grady
  * Created on 2017/12/12.
  */
+@org.springframework.stereotype.Controller
+@RequestMapping("/")
 public class Controller {
+
+    @RequestMapping("/")
+    public String test(Model model){
+        ModelMap modelMap = new ModelMap();
+        modelMap.addAttribute("name","gongyu");
+
+//        model.addAttribute("name","gongyu");
+        return "index";
+    }
 }
