419ef416c8e0141099338759d2211a4a171f74ec
Grady.Green <gongyu649424146@163.com>
2018-01-04 13:21:01 +0800

    security

---
+++ spring-security/src/main/java/com/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+    @GetMapping({"/index","/","/home"})
+    public String index(){
+        return "index";
+    }
+    @GetMapping("login")
+    public String login(){
+        return "login";
+    }
+}
